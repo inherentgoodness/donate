72582df3fdcf4a8f1089c50ba73efbb73327e32a
minzy <vminzluffy@gmail.com>
2019-08-25 06:11:38 +0530

    first commit

---
+++ precache-manifest.746a99464b3cf273335e26d935165759.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ce0efbad143947988175b3bfe3d8740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4f815ea1eab4fd5a2ec",
+    "url": "/static/css/main.645ebc47.chunk.css"
+  },
+  {
+    "revision": "6ca514b5162663fdc56b",
+    "url": "/static/js/2.f6078cd1.chunk.js"
+  },
+  {
+    "revision": "d4f815ea1eab4fd5a2ec",
+    "url": "/static/js/main.31240635.chunk.js"
+  },
+  {
+    "revision": "94a16c4968d074606799",
+    "url": "/static/js/runtime~main.a83500fb.js"
+  },
+  {
+    "revision": "894a5f036afa8ee955b9ed710d280753",
+    "url": "/static/media/Unicef_logo_blue.894a5f03.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27bbf3af48acdd88dc8aebf03268f83e",
+    "url": "/static/media/milaap.27bbf3af.png"
+  },
+  {
+    "revision": "b5be4469a6b7a306a4042ef863dc1033",
+    "url": "/static/media/unhcr3.b5be4469.PNG"
+  }
+]);
